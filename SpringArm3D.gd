361ed6399bd983f6ba2b723e5fd03f4e31ffd062
M3NTAL420 <Theoacoin@gmail.com>
2023-12-14 17:02:41 -0500

    addedCode

---
+++ SpringArm3D.gd
@@ -0,0 +1,18 @@
+extends SpringArm3D
+@export var mouse_sensitivity =0.005
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		# Called every frame. 'delta' is the elapsed time since the previous frame.
+		rotation.x -= event.relative.y * mouse_sensitivity
+		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(30))
+		
+		rotation.y -= event.relative.x * mouse_sensitivity
+		rotation.y = wrapf(rotation.y, -PI, PI)
+
+func _process(delta):
+	pass
